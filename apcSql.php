9ae3d21cbd87a563797d8a5a391aaea6ac5f1057
Roy Bellingan <roy@tech.techadsmedia.com>
2020-07-26 22:53:08 +0100

    init

---
+++ apcSql.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../phpMysql/dbwrapper.php';
+function apcCached(string $sql, DBWrapper $db, int $ttl = 60)
+{
+    //we do not store boolean never, so this is ok
+    $res = apcu_fetch($sql);
+    if ($res) {
+        return $res;
+    }
+    $res = $db->query($sql);
+    apc_store($sql,$res,$ttl);
+    return $res;
+}
