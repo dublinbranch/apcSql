030bb29b952fc1eee5db3650382cb39a494a0f2d
Roy Bellinga (s14) <roy@tech.techadsmedia.com>
2021-02-15 12:09:34 +0000

    include guard

--- apcSql.php
+++ apcSql.php
@@ -1,13 +1,15 @@
 <?php
-require_once __DIR__ . '/../phpMysql/dbwrapper.php';
-function apcCached(string $sql, DBWrapper $db, int $ttl = 60)
-{
-    //we do not store boolean never, so this is ok, at most we have NULL...
-    $res = apcu_fetch($sql);
-    if ($res !== false) {
-        return $res;
-    }
-    $res = $db->getLineSS($sql);
-    apcu_store($sql,$res,$ttl);
-    return $res;
+if(!function_exists("apcCached")){
+	require_once __DIR__ . '/../phpMysql/dbwrapper.php';
+	function apcCached(string $sql, DBWrapper $db, int $ttl = 60)
+	{
+	    //we do not store boolean never, so this is ok, at most we have NULL...
+	    $res = apcu_fetch($sql);
+	    if ($res !== false) {
+		return $res;
+	    }
+	    $res = $db->getLineSS($sql);
+	    apcu_store($sql,$res,$ttl);
+	    return $res;
+	}
 }
